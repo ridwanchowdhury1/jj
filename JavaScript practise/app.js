d44bd713e5633fdc56c7758135950d9d595c7df5
ridwanchowdhury1 <103056519+ridwanchowdhury1@users.noreply.github.com>
2023-02-15 22:56:17 +0600

    Add files via upload

---
+++ JavaScript practise/app.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+
+let count = 0;
+
+// while ( count < 100) {
+//     console.log(count)
+//     count++
+//     document.getElementById("one").innerHTML = (count);
+
+// }
+
+while (count < 100000 ) {
+    console.log(count);
+    count--
+}
